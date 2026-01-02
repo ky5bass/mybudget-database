0a3cbc2c85dc3fffd42916bdf38647d2ac3f5d03
ky5bass <yasuda.kanta.516@gmail.com>
2026-01-03 00:55:26 +0900

    2026年1月2日の更新

--- initdb/initdb.sql
+++ initdb/initdb.sql
@@ -1,15 +1,14 @@
-CREATE TABLE expenses (
+CREATE TABLE items (
     id SERIAL NOT NULL,
     name varchar(100) NOT NULL,
     price INTEGER NOT NULL,
-    is_earning BOOLEAN NOT NULL DEFAULT false,
-    date DATE,
+    date DATE DEFAULT NULL,
     PRIMARY KEY (id)
 );
 
-INSERT INTO expenses (name, price, is_earning, date) VALUES
-    ('水', 120, false, DATE '2025-11-20'),
-    ('パン', 460, false, DATE '2025-11-20'),
-    ('給料', 300000, true, DATE '2025-11-20'),
-    ('Tシャツ', 2000, false, DATE '2025-11-21'),
-    ('家賃', 100000, false, DATE '2025-11-21');
+INSERT INTO items (name, price, date) VALUES
+    ('水', -120, DATE '2025-11-20'),
+    ('パン', -460, DATE '2025-11-20'),
+    ('給料', 300000, DATE '2025-11-20'),
+    ('Tシャツ', -2000, NULL),
+    ('家賃', -100000, DATE '2025-11-21');
